a6c13992cabd7d62fb2563eb2ef09432ebdebad6
Spencer Massengale <spencer.massengale@aecc.com>
2019-08-07 14:28:54 -0500

    Added models & Admin Panel

--- pirate/pirates_inv/pirates/models.py
+++ pirate/pirates_inv/pirates/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Location(models.Model):
+    building = models.CharField(db_column='Building', primary_key= True, max_length=255)
+    room = models.CharField(db_column='Room', primary_key=True, max_length=255)
+
+    class Meta:
+        db_table = 'Location'
+        unique_together = (('building', 'room'),)
+
+
+class Vendor(models.Model):
+    vendor_name = models.CharField(db_column='Vendor', max_length=255):
+
+    class Meta:
+        db_table = 'vendor'
+
+
