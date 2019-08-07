ffa59f44b563a4625b89c8e9fdf78079485a7d42
Spencer Massengale <spencer.massengale@aecc.com>
2019-08-07 14:49:40 -0500

    Added to models

--- pirate/pirates_inv/pirates/models.py
+++ pirate/pirates_inv/pirates/models.py
@@ -2,12 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-
-
-
 class Location(models.Model):
     building = models.CharField(db_column='Building',  max_length=255)
-    room = models.CharField(db_column='Room', max_length=255)
+    room = models.CharField(db_column='Room', unique=True, max_length=255)
 
     class Meta:
         db_table = 'Location'
@@ -15,7 +12,7 @@ class Location(models.Model):
 
 
 class Vendor(models.Model):
-    vendor_name = models.CharField(db_column='Vendor', max_length=255)
+    vendor_name = models.CharField(db_column='Vendor', primary_key=True, max_length=255)
 
     class Meta:
         db_table = 'vendor'
