37d5dae78491e3b513b264fc5dc24a0b2478b13d
Spencer Massengale <spencer.massengale@aecc.com>
2019-08-07 17:01:11 -0500

    added vendor page

--- pirate/pirates_inv/pirates/urls.py
+++ pirate/pirates_inv/pirates/urls.py
@@ -15,14 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from pirates import views as pirates_views
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.views import View
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.dashboard, name='dashboard'),
-    path('dashboard/', views.dashboard, name = 'dashboard'),
+    path('', pirates_views.dashboard, name='dashboard'),
+    path('dashboard/', pirates_views.dashboard, name = 'dashboard'),
+    path('dashboard/vendors', pirates_views.vendors.as_view(), name = 'vendors'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
--- pirate/pirates_inv/pirates/views.py
+++ pirate/pirates_inv/pirates/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views import View
 # Create your views here.
+
+decorators = [never_cache, login_required]
 
 @login_required()
 def dashboard(request):
@@ -18,7 +24,7 @@ def dashboard(request):
         page_temp = 'pirates/dashboards/staff_dashboard.html'
     else:
         userType = 'Student'
-        page_temp = 'pirates/dashboards/admin_dashboard.html'
+        page_temp = 'pirates/dashboards/student_dashboard.html'
 
     context = {
         'user': str(user).title(),
@@ -26,3 +32,11 @@ def dashboard(request):
     }
 
     return render(request, page_temp, context)
+
+@method_decorator(decorators, name='dispatch')
+class vendors(UserPassesTestMixin, View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'pirates/elements/vendors.html')
+    def post(selfs, request, *args, **kwargs):
+        return render(request)
+
